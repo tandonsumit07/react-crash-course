e867eda92076331fff363c463c5fafdbf241e49b
Sumit Tandon <sumit.tandon@oattravel.com>
2024-10-02 13:45:51 +0530

    React Custom component

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+function Post() {
+    return <div>
+        <p>
+        Hi 
+        </p>
+        <p>
+            This is First React Component
+        </p>
+    </div>
+}
+
+export default Post
